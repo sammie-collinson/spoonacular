f971ea4233fe5c4c38589c70570616cb16bf1da0
Sammie Collinson <heroldsamantha@gmail.com>
2022-02-18 11:37:44 -0500

    added useEffect, made successful API call to spoontacular API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, {useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+
+  const APIKey = process.env.REACT_APP_SPOONACULAR_KEY
+  const API = `https://api.spoonacular.com/recipes/716429/information?apiKey=${APIKey}&includeNutrition=false`
+  const complexSearch = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKey}`
+
+  useEffect(() => {
+    async function getRecipes() {
+      const res = await axios.get(`${complexSearch}`)
+      console.log(res.data.results)
+    }
+    getRecipes();
+  })
+
   return (
     <div className="App">
       <h1>Hello</h1>
